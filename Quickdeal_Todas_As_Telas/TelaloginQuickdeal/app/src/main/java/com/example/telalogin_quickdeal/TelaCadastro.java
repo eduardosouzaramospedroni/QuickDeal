53baf73bb297fec2ec7c2774bc756071165ef3b3
eduardosouzaramospedroni <edusouzapedroni@gmail.com>
2025-10-26 18:47:43 -0300

    botao voltar

--- Quickdeal_Todas_As_Telas/TelaloginQuickdeal/app/src/main/java/com/example/telalogin_quickdeal/TelaCadastro.java
+++ Quickdeal_Todas_As_Telas/TelaloginQuickdeal/app/src/main/java/com/example/telalogin_quickdeal/TelaCadastro.java
@@ -1,6 +1,9 @@
 package com.example.telalogin_quickdeal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class TelaCadastro extends AppCompatActivity {
+    Button botao_voltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.tela_cadastro);
+
+        botao_voltar = findViewById(R.id.btn_voltar);
+
+        botao_voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mudartela = new Intent(TelaCadastro.this, TelaLogin.class);
+                startActivity(mudartela);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
